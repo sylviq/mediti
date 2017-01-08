f34ddc3eaee2ac350d46278b90cafcf671b77d47
artur <artur@unao.pl>
2017-01-08 15:57:19 +0100

    adds templates + and simple routing

--- app.js
+++ app.js
@@ -10,15 +10,48 @@ const config = {
 firebase.initializeApp(config);
 
 angular
-  .module('mediti', ['firebase'])
-  .config(() => {
+  .module('mediti', ['firebase', 'ngRoute'])
+  .config(($routeProvider, $locationProvider) => {
     console.log('hello from config');
+
+    $routeProvider
+      .when('/history', {
+        templateUrl: '/templates/history.html',
+        controller: 'HistoryCtrl'
+      })
+      .when('/session', {
+        templateUrl: '/templates/session.html',
+        controller: 'SessionCtrl'
+      })
+      .when('/profile', {
+        templateUrl: '/templates/profile.html',
+        controller: 'ProfileCtrl'
+      })
+      .when('/', {
+        templateUrl: '/templates/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({ redirectTo: '/' });
+
+    $locationProvider.html5Mode(true);
   })
   .controller('SimpleCtrl', ($scope, $firebaseObject) => {
-    var ref = firebase.database().ref().child("data");
-    var syncObject = $firebaseObject(ref);
+    const ref = firebase.database().ref().child("data");
+    const syncObject = $firebaseObject(ref);
     // synchronize the object with a three-way data binding
     // click on `index.html` above to see it used in the DOM!
     syncObject.$bindTo($scope, "data");
+  })
+  .controller('MainCtrl', function ($scope) {
+    console.log('hello from main-ctrl')
+  })
+  .controller('HistoryCtrl', function ($scope) {
+    console.log('HISTROY')
+  })
+  .controller('SessionCtrl', function ($scope, $routeParams) {
+
+  })
+  .controller('ProfileCtrl', function ($scope) {
+
   })
 ;
